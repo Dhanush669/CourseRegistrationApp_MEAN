f97a1884efaff136ca5fc4079ba86a2134063fb0
Dhanush669 <dhanushrukmangadhan@gmail.com>
2022-05-21 19:57:55 +0530

    migrated from local mongodb to mongodb atlas

--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,16 @@
 const mongo=require("mongoose")
 require('dotenv').config()
-mongo.connect("mongodb://localhost:27017/CourseRegistratonApp")
+//mongo.connect("mongodb://localhost:27017/CourseRegistratonApp")
+const db="mongodb+srv://root:Litpassword@cluster0.xxju2.mongodb.net/CourseRegistrationApp?retryWrites=true&w=majority"
+mongo.connect(db,{
+    useNewUrlParser:true,
+    
+    useUnifiedTopology:true,
+    
+}).then(()=>{
+    console.log("connection successfull");
+}).catch((err)=>{
+    console.log("err "+err);
+})
 
 module.exports=mongo
